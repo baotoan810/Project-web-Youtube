6b54bbfe8827190f8fb8f34ceb9cc177c3308f5a
baotoan810 <you@example.com>
2024-07-17 22:19:21 +0700

    up code 1

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,5 +1,25 @@
-const menuButton = document.querySelector(".menu-button");
+const menuButton = document.querySelectorAll(".menu-button");
+const screenOverlay = document.querySelector(".screen-overplay");
+const themeButton = document.querySelector(".theme-button");
 
-menuButton.addEventListener("click", () => {
+themeButton.addEventListener("click", () => {
+    const isDarkMode = document.body.classList.toggle("dark-mode")
+    themeButton.classList.toggle("uil-sun", isDarkMode);
+    themeButton.classList.toggle("uil-moon", !isDarkMode);
+
+});
+
+menuButton.forEach(button => {
+    button.addEventListener("click", () => {
+        document.body.classList.toggle("sidebar-hidden")
+    });
+});
+
+
+screenOverlay.addEventListener("click", () => {
     document.body.classList.toggle("sidebar-hidden")
 });
+
+if (window.innerWidth >= 768) {
+    document.body.classList.remove("sidebar-hidden")
+}
